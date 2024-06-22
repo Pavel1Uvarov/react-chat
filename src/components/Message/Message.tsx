f2af3d1152643e780d83fa7e96a7999c4a50b4e5
unknown <pavel2uvarov@gmail.com>
2024-06-22 16:43:41 +0200

    feat(UI): update ui

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,17 +1,24 @@
 import { cn, formatDate } from "@/lib/utils";
 import { selectUser, useUserStore } from "@/stores/user.store";
 import { IMessage } from "@/types/message.interface";
+import { useMemo } from "react";
 
 const Message = ({ message }: { message: IMessage }) => {
   const userId = useUserStore(selectUser)?.id;
+  const isCurrentUser = useMemo(() => {
+    return userId === message.user_id;
+  }, [userId, message]);
+
   return (
-    <div
-      className={cn(
-        "w-full flex",
-        userId === message.user_id ? "justify-end" : ""
-      )}
-    >
-      <div className="flex flex-col bg-gradient-to-r from-indigo-500 to-indigo-600 rounded p-4 w-[70%] text-white">
+    <div className={cn("w-full flex", isCurrentUser ? "justify-end" : "")}>
+      <div
+        className={cn(
+          "flex flex-col rounded p-4 w-[70%] text-white bg-gradient-to-r",
+          isCurrentUser
+            ? "from-blue-800 to-indigo-900"
+            : "from-slate-900 to-slate-700"
+        )}
+      >
         <div className="break-all font-light">{message.text}</div>
         <time
           className="text-xs flex justify-end text-slate-200 mt-3 font-bold"
