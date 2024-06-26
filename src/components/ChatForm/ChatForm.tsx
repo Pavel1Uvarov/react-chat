f58d6841bbedf7038ec69769909abd33b84c6a7d
Pavel Uvarov <paveluvarov@Pavels-MacBook-Air.local>
2024-06-26 20:46:53 +0200

    feat(ChatForm): remvove unused

--- src/components/ChatForm/ChatForm.tsx
+++ src/components/ChatForm/ChatForm.tsx
@@ -1,7 +1,6 @@
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { PaperPlaneIcon } from "@radix-ui/react-icons";
-import { Loader2 } from "lucide-react";
 import { useChatForm } from "./_chatForm.hook";
 import Spinner from "../Spinner/Spinner";
 
