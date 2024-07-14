4a9c3252af96ad25849b2fd99f465c6b440237d1
Pavel1Uvarov <pavel2uvaorv@gmail.com>
2024-07-14 17:55:42 +0200

    feat(Supabase): add types

--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -1,4 +1,5 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { Database } from "@/types/supabase.types.ts";
 
 const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL ?? "";
 const supabaseAnonKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY ?? "";
@@ -6,7 +7,7 @@ const supabaseAnonKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY ?? "";
 let supabase;
 
 if (supabaseUrl && supabaseAnonKey) {
-  supabase = createClient(supabaseUrl, supabaseAnonKey);
+  supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
 }
 
 export default supabase as SupabaseClient;
